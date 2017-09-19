776baf0c90061f5586613c1bd639083150dcdbf9
Харьков Иван <31773879+Harek98@users.noreply.github.com>
2017-09-19 21:54:48 +0500

    Add files via upload

---
+++ Вариант №4. Задание 1/Вариант №4. Задание 1/Вариант №4. Задание 1.cpp
@@ -0,0 +1,26 @@
+// Вариант №4. Задание 1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#define PI 3.14159265
+using namespace std;
+
+
+int main()
+{
+	int x, y, z;
+	double a, b;
+	cout << "Enter x" << endl;
+	cin >> x;
+	cout << "Enter y" << endl;
+	cin >> y;
+	cout << "Enter z" << endl;
+	cin >> z;
+	a = ((2 * cos(x - (PI / 4)))) / (1 / 2 * (sin(y))) + acos(x + PI / 2);
+	b = 1 + ((z * z) / (3 + ((z * z) / 5)));
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
+	system("pause");
+}
+
